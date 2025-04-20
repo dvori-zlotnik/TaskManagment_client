53f0466ea3e0e2f1a1c19f5ba2c228dd86001283
dvori <dvori7330@gmail.com>
2025-04-20 23:58:56 +0300

    change baseurl

--- src/service.js
+++ src/service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const my_axios = axios.create({
-  baseURL: 'http://localhost:5112',
+  baseURL: process.env.REACT_APP_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
